e736432dc3f9735efe00e72ad849454cfd37f656
Andrey Simonov <simonov@proitr.ru>
2016-10-21 16:09:19 +0500

    cache time

--- inline.py
+++ inline.py
@@ -2,6 +2,7 @@
 
 import os
 import time
+import uuid
 import asyncio
 import telepot
 import telepot.aio
@@ -23,8 +24,8 @@ async def on_chat_message(msg):
         await bot.sendMessage(chat_id, hangoutLinker.getlink())
 
 
-def on_inline_query(msg):
-    def compute_answer():
+async def on_inline_query(msg):
+    async def compute_answer():
             query_id, from_id, query_string = telepot.glance(msg, flavor='inline_query')
             print('Inline Query:', query_id, from_id, query_string)
 
@@ -37,21 +38,24 @@ def on_inline_query(msg):
 
             articles = [{
                     'type': 'article',
-                    'id': 'telehangbot_id', 
-                    'title': 'Your message: ' + query_string, 
+                    'id': str(uuid.uuid4()), 
+                    'thumb_url': 'http://zags.interfacetesting.ru/lavrov.jpg',
+                    'title': 'Начать сложные переговоры с шутки: ' + query_string, 
                     'message_text': query_string + '\r\n' + uri
                 }]
 
             return articles
 
-    answerer.answer(msg, compute_answer)
+    result = await compute_answer()
+    await bot.answerInlineQuery(msg['id'], result, 1)
 
 
 def on_chosen_inline_result(msg):
     result_id, from_id, query_string = telepot.glance(msg, flavor='chosen_inline_result')
     print('Chosen Inline Result:', result_id, from_id, query_string)
     #remove link from cache
-    link_by_user.pop(from_id)
+    if link_by_user.get(from_id) is not None:
+        link_by_user.pop(from_id)   
 
 #take some time to start selenium server
 time.sleep(30)
@@ -63,7 +67,8 @@ loop = asyncio.get_event_loop()
 
 loop.create_task(bot.message_loop({'chat': on_chat_message,
                                    'inline_query': on_inline_query,
-                                   'chosen_inline_result': on_chosen_inline_result}))
+                                   'chosen_inline_result': on_chosen_inline_result
+                                   }))
 print('Listening ...')
 
 loop.run_forever()
